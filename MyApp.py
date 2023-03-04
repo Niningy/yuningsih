73c00d95be6169b69d9327e6ab84ec4b0dbf34ab
Niningy <116931859+Niningy@users.noreply.github.com>
2023-03-04 11:05:46 +0700

    Add files via upload

---
+++ MyApp.py
@@ -0,0 +1,52 @@
+#import the required Libraries streamlit import st
+
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+# Add a tittle and intro text
+st.title('CURRICULUM VITAE')
+
+# image
+st.image('image.jpg')
+st.header('Telp: +6285817253707 / wa.me/+6285817253707')
+
+st.title('NINING YUNINGSIH')
+
+st.header('ADUCATION')
+st.text('Universitas Tebuka(UT) Bachelor of Accounting 2018-2023')
+st.text('High School SMA PANDU IPA Passed 2011')
+st.text('MTS TSANAWIAH Passed 2008')
+st.text('MI SIRAJUL HUDA Passed 2007')
+
+st.header('NON FORMAL EDUCATION')
+st.text('1. Training Course Computer High School')
+st.text('2. Chief of Kadiv.Progrram Seminar Education Youth Meeting')
+st.text('3. English Club')
+st.text('4. TOEFL')
+st.text('5. Training Course online SAP MM and SAP Netweaver ABAP Program')
+st.text('6.Training Course Python Progrraming')
+
+
+st.header('JOB EXPERIENCES')
+st.text('1.Briton School Of English (BSE) Course and Tranlation Bording Position Administration - South Jakarta 2012-2013')
+st.text('2.PT.Garuda Ekspress Delivary (Ekspedition Company) Customer services & Sales Outlate Position South Jakarta 2013-2014')
+st.text('3.PT.Carefastindo Last Position Administrasi Support Logistic West Jakarta 2014-2020')
+st.text('4.PT.HIJAU BERDIKARI INDONESIA last Position Administrasi Opeerational')
+st.text('5.Founder Wijayakusumasport online shop umkm Bogor West Java 2020-2021')
+st.text('6.Sales\Marketing Online 2021-Now')
+st.text('7.freelancer Tutor in the PKBM Bording School 2022-Now')
+
+st.header('Certificates & Awards')
+st.text('1.Certificate Computer program(Ms.Word,Ms.Ecxel,Ms.Power Point)')
+st.text('2.Certificate TOEFL of Eligmu Course')
+st.text('3.Certificate Royalti Award 2019 of PT.Carefastindo & Congratulation Achieve Business of PT.Carefastindo')
+st.text('4.Certificate SAP MM of BelajarSAP course')
+st.text('5.Certificate Auditor 3 SKP of Auditor Indonesia')
+st.text('6.Certificate networking Academy(Cybersecurity and Cyber Esentials"CISO")')
+st.text('7. Certificate Akuntansi 2 SKP of open University indonesia(Universitas Terbuka(UT))')
+st.text('8.Certificate call paper of the P3EI(Pusat Pengkajian Dan Pengembangan Ekonomi Islam)')
+st.text('9.Certificate training UMKM class start up Biz-UMKM(Usaha Kecil Menengah )')
+
+
+
